1d14c09c82e09de92a91000cc47faf53ac6262e8
C0B23013 <c0b23013f7@edu.teu.ac.jp>
2024-10-01 17:15:15 +0900

    演習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -11,11 +11,14 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def game_over(screen, txt, fonto):
+    """
+    ゲームオーバーと表示させるリストを作成
+    """
     black = pg.Surface(screen.get_size())  # 画面サイズのSurfaceを作成
     black.fill((0, 0, 0))  # 黒く塗りつぶす
     black.set_alpha(150)  # 半透明に
     
-    screen.blit(black, (0, 0))  # 画面に描画（ブラックアウト）
+    screen.blit(black, (0, 0))  # 画面に描画
     screen.blit(txt, (530, 150))  # 泣いているこうかとんの画像を表示
     
     # Game Overの文字列を表示
@@ -41,6 +44,21 @@ def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
     return yoko, tate   
 
 
+def bombtime():
+    """
+    爆弾のサイズと加速度を段階的に変化させるリストを作成
+    """
+    bb_imgs = []
+    bb_accs = [a for a in range(1, 11)]  # 加速度のリスト 
+    
+    for r in range(1, 11):  # 爆弾のサイズを1倍から10倍まで作成
+        bb_img = pg.Surface((20*r, 20*r))  # サイズに応じたSurface作成
+        bb_img.set_colorkey((0, 0, 0))  
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)  # 赤い円
+        bb_imgs.append(bb_img)
+    
+    return bb_imgs, bb_accs
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -48,10 +66,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    bb_img = pg.Surface((20, 20))  # 空のSurface
-    bb_img.set_colorkey((0, 0, 0))  # 爆弾の四隅を透過させる
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    bb_rct = bb_img.get_rect()  # 爆弾Rectの抽出
+
+    bb_imgs, bb_accs = bombtime()
+
+    #bb_img = pg.Surface((20, 20))  # 空のSurface
+    #bb_img.set_colorkey((0, 0, 0))  # 爆弾の四隅を透過させる
+    #pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_rct = bb_imgs[0].get_rect()  # 爆弾Rectの抽出
     bb_rct.centerx = random.randint(0, WIDTH)
     bb_rct.centery = random.randint(0, HEIGHT)
     vx, vy = +5, +5  # 爆弾の速度
@@ -71,7 +92,10 @@ def main():
             game_over(screen, txt, fonto)
             #print("GameOver")
             return
-        
+           
+        avx = vx * bb_accs[min(tmr // 500, 9) ]  
+        avy = vy * bb_accs[min(tmr // 500, 9) ]     
+        bb_img = bb_imgs[min(tmr // 500, 9) ]  
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]  #横座標 縦座標
@@ -92,12 +116,14 @@ def main():
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)
+
+        bb_rct.move_ip(avx, avy)
         yoko, tate = check_bound(bb_rct)
         if not yoko:
             vx *= -1
         if not tate:
             vy *= -1
+
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
